be92c40589eaf532badeef3133aec04f075ba6b9
Yevhen Chernytskyi <eugene.chernitskiy@gmail.com>
2025-05-16 17:14:14 -0400

    fix

--- server.js
+++ server.js
@@ -14,6 +14,7 @@
 
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const SitesDB = require("./modules/sitesDB.js");
 
 const app = express();
